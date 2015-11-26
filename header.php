5a0eca577bba39a965a57ddb74d9f7d6ed8ae7bd
ko31 <knockoutmarch@gmail.com>
2015-11-26 14:39:01 +0900

    Changed title tag

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>pigeon</title>
+<title><?php echo get_bloginfo( 'name' ); ?></title>
 <?php wp_head(); ?>
 </head>
 
