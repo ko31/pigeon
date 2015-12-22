59f5fbe9d2ac7025225dfe7e2fe5bee5f38651f3
ko31 <knockoutmarch@gmail.com>
2015-12-23 02:25:34 +0900

    Added favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <title><?php echo get_bloginfo( 'name' ); ?></title>
 <?php wp_head(); ?>
 </head>
