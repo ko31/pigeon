0aa3d4d33aa79c6b4c7ec6a4e73f929c37303dfc
ko31 <knockoutmarch@gmail.com>
2015-12-26 22:02:03 +0900

    Add tags for home icons of smartphone

--- header.php
+++ header.php
@@ -4,7 +4,11 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+<meta name="mobile-web-app-capable" content="yes">
+<link rel="icon" sizes="192x192" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" />
+<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" />
 <title><?php echo get_bloginfo( 'name' ); ?></title>
 <?php wp_head(); ?>
 </head>
